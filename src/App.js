487aef39703899fafb8cce54bb43bdafe7c76ae2
Anko <anna.jedrzejko@op.pl>
2021-06-09 19:00:01 +0200

    feat: Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import HomePage from './pages/HomePage';
+import SeatsPage from './pages/SeatsPage';
+import ResultPage from './pages/ResultPage';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/miejsca" component={SeatsPage} />
+          <Route path="/podsumowanie" component={ResultPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/SeatsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeatsPage = () => {
+    return (
+        <div>Seatspage</div>
+    );
+}
+
+export default SeatsPage;
