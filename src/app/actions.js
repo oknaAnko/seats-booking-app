415584dceefd63d2e6dc107972a1642c44557d6b
Anko <anna.jedrzejko@op.pl>
2021-07-01 16:54:46 +0200

    refactor: Action types

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,31 +1,24 @@
 import axios from 'axios';
-import { store } from './store';
-
-export const FETCH_SEATS_REQUEST = "FETCH_SEATS_REQUEST";
-export const FETCH_SEATS_SUCCESS = "FETCH_SEATS_SUCCESS";
-export const FETCH_SEATS_FAIL = "FETCH_SEATS_FAIL";
-export const TOGGLE_CHOOSEN_SEATS = "TOGGLE_CHOOSEN_SEATS";
-export const RESERVE_SEATS = "RESERVE_SEATS";
-
+import { types } from './constants';
 
 const seatsFetchedRequest = () => ({
-    type: 'FETCH_SEATS_REQUEST',
+    type: types.FETCH_SEATS_REQUEST,
 });
 
 const seatsFetchedSuccess = seats => ({
-    type: 'FETCH_SEATS_SUCCESS',
+    type: types.FETCH_SEATS_SUCCESS,
     payload: seats
 });
 
 const seatsFetchedFail = error => ({
-    type: 'FETCH_SEATS_FAIL',
+    type: types.FETCH_SEATS_FAIL,
     payload: error
 });
 
 export const fetchSeats = () => dispatch => {
     dispatch(seatsFetchedRequest());
 
-    axios.get('http://localhost:3004/seats')
+    return axios.get('http://localhost:3004/seats')
         .then(res => res.data)
         .then(seats => {
             const seatsWithChosenProperty = seats.map(seat => ({ ...seat, chosen: false }))
@@ -36,14 +29,12 @@ export const fetchSeats = () => dispatch => {
         })
 };
 
-store.dispatch(fetchSeats());
-
 export const toggleChosenSeats = seatIds => ({
-    type: 'TOGGLE_CHOOSEN_SEATS',
+    type: types.TOGGLE_CHOSEN_SEATS,
     payload: seatIds
 })
 
 export const reserveSeats = seatIds => ({
-    type: 'RESERVE_SEATS',
+    type: types.RESERVE_SEATS,
     payload: seatIds
 })
