55bdc5f137470c8c29d9e580138aed2d1dfedebf
Anko <anna.jedrzejko@op.pl>
2022-05-17 21:05:53 +0200

    refactor: change location of squares

--- src/components/OneSeat.js
+++ src/components/OneSeat.js
@@ -1,37 +1,27 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleChosenSeats } from '../app/actions';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleChosenSeats } from "../app/actions";
 
 const OneSeat = ({ id }) => {
+  const seat = useSelector((state) => state.seats.seats.find((seat) => seat.id === id));
 
-    const seat = useSelector(state => state.seats.seats.find(seat => seat.id === id));
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const handleChooseSeatClick = () => {
+    if (!seat.reserved) dispatch(toggleChosenSeats([id]));
+  };
 
-    const handleChooseSeatClick = () => {
-        if (!seat.reserved)
-            dispatch(toggleChosenSeats([id]));
-    };
+  let color = "transparent";
+  if (seat.reserved) color = "grey";
+  if (seat.chosen) color = "orange";
 
-    let color = "transparent";
-    if (seat.reserved) color = "grey";
-    if (seat.chosen) color = "orange";
-
-
-    return (
-        <div
-            id={id}
-            className="square"
-            style={{ top: seat.cords.x * 60, left: seat.cords.y * 60 }}>
-            <button
-                className="seat"
-                style={{ backgroundColor: color }}
-                onClick={handleChooseSeatClick}
-            >
-                {id}
-            </button>
-        </div>
-    );
-}
+  return (
+    <div id={id} className="square" style={{ top: seat.cords.x * 40, left: seat.cords.y * 40 }}>
+      <button className="seat" style={{ backgroundColor: color }} onClick={handleChooseSeatClick}>
+        {/* {id} */}
+      </button>
+    </div>
+  );
+};
 
 export default OneSeat;
